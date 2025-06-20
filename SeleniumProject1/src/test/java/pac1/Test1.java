5bf78d036f166bbad3dfd14258c94855ada61aef
sadiyass751 <ss7106521@gmail.com>
2025-06-20 07:13:29 +0530

    commit

---
+++ SeleniumProject1/src/test/java/pac1/Test1.java
@@ -0,0 +1,19 @@
+package pac1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test(groups="Regression")
+	public void test() {
+		
+	}
+	
+	@Test(groups="Regression")
+	public void test1() {
+		System.out.println("hi");
+	}
+
+}
---
+++ SeleniumProject1/src/test/java/pac2/Test2.java
@@ -0,0 +1,22 @@
+package pac2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test(groups={"Smoke","Regression"})
+	public void demo() {
+		String url=System.getProperty("url");
+		String browser=System.getProperty("browser","chrome");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		System.out.println(url+"\n"+browser+username+password);
+	}
+	
+	@Test(groups="Regression")
+	public void demo1() {
+		System.out.println("demo");
+	}
+}
